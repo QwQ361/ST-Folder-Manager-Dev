58fddf22e1198b47435e6e61be512dd314526d6f
QwQ361 <lin41758293@qq.com>
2026-03-28 21:23:58 +0800

    回滚

--- index.js
+++ index.js
@@ -32326,16 +32326,9 @@ jQuery(async () => {
       }
     });
 
-    dialog.find(".cfm-sort-dialog-cancel").on("click touchend", async (e) => {
+    dialog.find(".cfm-sort-dialog-cancel").on("click touchend", (e) => {
       e.preventDefault();
       e.stopPropagation();
-      try {
-        if (typeof onSkip === "function") {
-          await onSkip();
-        }
-      } catch (err) {
-        console.error("[CFM] 取消局部正则插入排序失败:", err);
-      }
       closeDialog();
     });
 
@@ -33313,8 +33306,6 @@ jQuery(async () => {
       dialog.find(".cfm-sort-dialog-cancel").on("click touchend", (e) => {
         e.preventDefault();
         e.stopPropagation();
-        renderRegexView();
-        toastr.success("新正则已创建，顺序保持在最后");
         closeDialog();
       });
     } else {
